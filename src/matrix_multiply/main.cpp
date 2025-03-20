dc416f8ae64fb9910ab2d44c770b3cff1b92ac70
Shihao Tian <shihao.tian@outlook.com>
2025-03-20 19:46:31 +0800

    feat(matrix_multiply): calculate the bandwidth of matrix multiply and format the code

--- src/matrix_multiply/main.cpp
+++ src/matrix_multiply/main.cpp
@@ -1,31 +1,42 @@
+#include <cstddef>
 #include <iostream>
 #include <vector>
 
 #include "matrix_multiply.h"
 
 int main() {
-  constexpr int A_rows{160};  // Number of rows in matrix A
-  constexpr int A_cols{
-      cuda_lab::matrix_multiply::TILE_DIM};  // Number of columns in matrix A
-  constexpr int B_cols{192};                 // Number of columns in matrix B
-  std::vector<float> h_a(A_rows * A_cols,
-                         1.0f);  // Initialize matrix A with 1.0
-  std::vector<float> h_b(A_cols * B_cols,
-                         2.0f);  // Initialize matrix B with 2.0
-  std::vector<float> h_c(A_rows * B_cols,
+//   constexpr std::size_t a_rows{160};  // Number of rows in matrix A
+//   constexpr std::size_t a_cols{
+//       cuda_lab::matrix_multiply::TILE_DIM};  // Number of columns in matrix A
+//   constexpr std::size_t b_cols{192};         // Number of columns in matrix B
+//   std::vector<float> h_a(a_rows * a_cols,
+//                          1.0f);  // Initialize matrix A with 1.0
+//   std::vector<float> h_b(a_cols * b_cols,
+//                          2.0f);  // Initialize matrix B with 2.0
+//   std::vector<float> h_c(a_rows * b_cols,
+//                          0.0f);  // Initialize matrix C with 0.0
+
+  constexpr std::size_t a_rows{2};  // Number of rows in matrix A
+  constexpr std::size_t a_cols{3};  // Number of columns in matrix A
+  constexpr std::size_t b_cols{4};  // Number of columns in matrix B
+  std::vector<float> h_a{1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f};
+  std::vector<float> h_b{1.0f, 2.0f, 3.0f, 4.0f,  5.0f,  6.0f,
+                         7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f};
+
+  std::vector<float> h_c(a_rows * b_cols,
                          0.0f);  // Initialize matrix C with 0.0
 
   cuda_lab::matrix_multiply::PrintMatrix(
-      h_a.data(), A_rows, A_cols, cuda_lab::matrix_multiply::TILE_DIM, "A");
+      h_a.data(), a_rows, a_cols, cuda_lab::matrix_multiply::TILE_DIM, "A");
   cuda_lab::matrix_multiply::PrintMatrix(
-      h_b.data(), A_cols, B_cols, cuda_lab::matrix_multiply::TILE_DIM, "B");
+      h_b.data(), a_cols, b_cols, cuda_lab::matrix_multiply::TILE_DIM, "B");
 
   cuda_lab::matrix_multiply::MatrixMultiply(
-      h_a.data(), h_b.data(), h_c.data(), A_rows, A_cols, B_cols,
-      cuda_lab::matrix_multiply::MatrixMultiplyType::kCoalesced);
+      h_a.data(), h_b.data(), h_c.data(), a_rows, a_cols, b_cols,
+      cuda_lab::matrix_multiply::MatrixMultiplyType::kSharedAB);
 
   cuda_lab::matrix_multiply::PrintMatrix(
-      h_c.data(), A_rows, B_cols, cuda_lab::matrix_multiply::TILE_DIM, "C");
+      h_c.data(), a_rows, b_cols, cuda_lab::matrix_multiply::TILE_DIM, "C");
 
   return 0;
 }
--- src/matrix_multiply/matrix_multiply.cu
+++ src/matrix_multiply/matrix_multiply.cu
@@ -7,34 +7,45 @@
 
 namespace cuda_lab::matrix_multiply {
 
-__global__ void CoalescedMultiply(float* a, float* b, float* c, int A_rows,
-                                  int A_cols, int B_cols) {
+__global__ void CoalescedMultiply(float const* const a, float const* const b,
+                                  float* const c, std::size_t const a_rows,
+                                  std::size_t const a_cols,
+                                  std::size_t const b_cols) {
   // Share memory on chip
   __shared__ float a_tile[TILE_DIM][TILE_DIM];
 
   auto row{blockIdx.y * blockDim.y + threadIdx.y};
   auto col{blockIdx.x * blockDim.x + threadIdx.x};
-  auto sum{0.0f};
+
+  if (row >= a_rows || col >= b_cols) {
+    return;
+  }
 
   // Cache all rows in the block.
-  a_tile[threadIdx.y][threadIdx.x] = a[row * A_cols + threadIdx.x];
+  a_tile[threadIdx.y][threadIdx.x] = a[row * a_cols + threadIdx.x];
 
   __syncwarp();
 
+  auto sum{0.0f};
+
   // i to iterate over the columns of A and rows of B.
-  for (int i{0}; i < A_cols; i++) {
-    sum += a_tile[threadIdx.y][i] * b[i * B_cols + col];
+  for (std::size_t i{0}; i < a_cols; ++i) {
+    sum += a_tile[threadIdx.y][i] * b[i * b_cols + col];
   }
-  c[row * B_cols + col] = sum;
+  c[row * b_cols + col] = sum;
 }
 
-__global__ void SimpleMultiply(float* a, float* b, float* c, int A_rows,
-                               int A_cols, int B_cols) {
+__global__ void SimpleMultiply(float* a, float* b, float* c, int a_rows,
+                               int a_cols, int b_cols) {
   auto row{blockIdx.y * blockDim.y + threadIdx.y};
   auto col{blockIdx.x * blockDim.x + threadIdx.x};
 
+  if (row >= a_rows || col >= b_cols) {
+    return;
+  }
+
   // Output sample kernel info
-  if (row == A_rows / 2 && col == B_cols / 2) {
+  if (row == a_rows / 2 && col == b_cols / 2) {
     printf("Sample kernel info:\n");
     printf(
         "Block (%d, %d), Thread (%d, %d)\nRow = blockIdx.y * blockDim.y + "
@@ -44,68 +55,115 @@ __global__ void SimpleMultiply(float* a, float* b, float* c, int A_rows,
         blockDim.y, threadIdx.y, row, blockIdx.x, blockIdx.y, threadIdx.x, col);
   }
 
-  if (row < A_rows && col < B_cols) {
-    auto sum{0.0f};
-    for (int i{0}; i < A_cols; i++) {
-      sum += a[row * A_cols + i] * b[i * B_cols + col];
-    }
-    c[row * B_cols + col] = sum;
+  auto sum{0.0f};
+  for (std::size_t i{0}; i < a_cols; ++i) {
+    sum += a[row * a_cols + i] * b[i * b_cols + col];
   }
+  c[row * b_cols + col] = sum;
 }
 
-void MatrixMultiply(float* h_a, float* h_b, float* h_c, int A_rows, int A_cols,
-                    int B_cols, MatrixMultiplyType type) {
+void MatrixMultiply(float const* const h_a, float const* const h_b,
+                    float* const h_c, std::size_t const a_rows,
+                    std::size_t const a_cols, std::size_t const b_cols,
+                    MatrixMultiplyType const type) {
   float *d_a{nullptr}, *d_b{nullptr}, *d_c{nullptr};
 
   // Allocate device memory
-  auto err{cudaMalloc((void**)&d_a, A_rows * A_cols * sizeof(float))};
+  auto err{cudaMalloc((void**)&d_a, a_rows * a_cols * sizeof(float))};
   CheckCudaError(err, "Failed to allocate device memory for d_a");
 
-  err = cudaMalloc((void**)&d_b, A_cols * B_cols * sizeof(float));
+  err = cudaMalloc((void**)&d_b, a_cols * b_cols * sizeof(float));
   CheckCudaError(err, "Failed to allocate device memory for d_b");
 
-  err = cudaMalloc((void**)&d_c, A_rows * B_cols * sizeof(float));
+  err = cudaMalloc((void**)&d_c, a_rows * b_cols * sizeof(float));
   CheckCudaError(err, "Failed to allocate device memory for d_c");
 
   // Transfer data from host to device
-  err = cudaMemcpy(d_a, h_a, A_rows * A_cols * sizeof(float),
+  err = cudaMemcpy(d_a, h_a, a_rows * a_cols * sizeof(float),
                    cudaMemcpyHostToDevice);
   CheckCudaError(err, "Failed to copy h_a to d_a");
 
-  err = cudaMemcpy(d_b, h_b, A_cols * B_cols * sizeof(float),
+  err = cudaMemcpy(d_b, h_b, a_cols * b_cols * sizeof(float),
                    cudaMemcpyHostToDevice);
   CheckCudaError(err, "Failed to copy h_b to d_b");
 
   dim3 block_size(TILE_DIM, TILE_DIM);
-  dim3 grid_size(DivUp(B_cols, TILE_DIM), DivUp(A_rows, TILE_DIM));
+  dim3 grid_size(DivUp(b_cols, TILE_DIM), DivUp(a_rows, TILE_DIM));
 
   // Print kernel info
   std::cout << "Kernel info: " << std::endl;
-  std::cout << "Data size: " << "row: " << A_rows << " x col: " << B_cols
+  std::cout << "Data size: " << "row: " << a_rows << " x col: " << b_cols
             << std::endl;
-  std::cout << "Element number: " << A_rows * B_cols << std::endl;
+  std::cout << "Element number: " << a_rows * b_cols << std::endl;
   std::cout << "Block size: " << block_size.x << " x " << block_size.y
             << std::endl;
   std::cout << "Grid size: " << grid_size.x << " x " << grid_size.y << std::endl
             << std::endl;
 
+  // Create CUDA events
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+
+  // Record the start event
+  cudaEventRecord(start, 0);
+
   // Calling the kernel
   switch (type) {
+    case MatrixMultiplyType::kSimple: {
+      SimpleMultiply<<<grid_size, block_size>>>(d_a, d_b, d_c, a_rows, a_cols,
+                                                b_cols);
+      break;
+    }
+    case MatrixMultiplyType::kCoalesced: {
+      CoalescedMultiply<<<grid_size, block_size>>>(d_a, d_b, d_c, a_rows,
+                                                   a_cols, b_cols);
+      break;
+    }
+    default:
+      break;
+    default: {
+      break;
+    }
+  }
+
+  // Record the stop event
+  cudaEventRecord(stop, 0);
+
+  // Wait for the stop event to complete
+  cudaEventSynchronize(stop);
+
+  switch (type) {
     case MatrixMultiplyType::kSimple:
-      SimpleMultiply<<<grid_size, block_size>>>(d_a, d_b, d_c, A_rows, A_cols,
-                                                B_cols);
+      std::cout << "SimpleMultiply executed." << std::endl;
       break;
 
     case MatrixMultiplyType::kCoalesced:
-      CoalescedMultiply<<<grid_size, block_size>>>(d_a, d_b, d_c, A_rows,
-                                                   A_cols, B_cols);
+      std::cout << "CoalescedMultiply executed." << std::endl;
       break;
 
     default:
       break;
   }
-  // SimpleMultiply<<<grid_size, block_size>>>(d_a, d_b, d_c, A_rows, A_cols,
-  //                                           B_cols);
+
+  // Calculate the elapsed time
+  float elapsed_time;
+  cudaEventElapsedTime(&elapsed_time, start, stop);
+  std::cout << "Kernel execution time: " << elapsed_time << " ms" << std::endl;
+
+  // Calculate bandwidth
+  // A and B matrices
+  float data_size_gb{2.0f * a_rows * a_cols * sizeof(float) /
+                     (1024.0f * 1024.0f * 1024.0f)};
+  data_size_gb += a_rows * b_cols * sizeof(float) /
+                  (1024.0f * 1024.0f * 1024.0f);             // C matrix
+  float bandwidth{data_size_gb / (elapsed_time / 1000.0f)};  // Convert ms to s
+  std::cout << "Kernel bandwidth: " << bandwidth << " GB/s" << std::endl
+            << std::endl;
+
+  // Destroy CUDA events
+  cudaEventDestroy(start);
+  cudaEventDestroy(stop);
 
   cudaStreamSynchronize(0);  // Wait for the kernel to finish
   // Check for kernel errors
@@ -113,7 +171,7 @@ void MatrixMultiply(float* h_a, float* h_b, float* h_c, int A_rows, int A_cols,
   CheckCudaError(err, "Kernel execution failed");
 
   // Transfer data from device to host
-  err = cudaMemcpy(h_c, d_c, A_rows * B_cols * sizeof(float),
+  err = cudaMemcpy(h_c, d_c, a_rows * b_cols * sizeof(float),
                    cudaMemcpyDeviceToHost);
   CheckCudaError(err, "Failed to copy d_c to h_c");
 
