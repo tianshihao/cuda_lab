27b11c6533c134d38a158f8892dc5c58c66a9354
Shihao Tian <shihao.tian@outlook.com>
2025-04-14 10:56:47 +0800

    refactor(matrix_multiply): refactor the code

--- src/matrix_multiply/main.cpp
+++ src/matrix_multiply/main.cpp
@@ -7,26 +7,8 @@
 #include "matrix.h"
 #include "matrix_multiply.h"
 
-// void SetMatrix2(cuda_lab::matrix_multiply::MatrixF &h_a,
-//                 cuda_lab::matrix_multiply::MatrixF &h_b,
-//                 cuda_lab::matrix_multiply::MatrixF &h_c) {
-//   constexpr std::size_t a_rows{16000};
-//   constexpr std::size_t a_cols{cuda_lab::matrix_multiply::BLOCK_SIZE};
-//   constexpr std::size_t b_rows{a_cols};
-//   constexpr std::size_t b_cols{19200};
-
-//   h_a = cuda_lab::matrix_multiply::MatrixF(a_rows, a_cols, a_cols);
-//   h_b = cuda_lab::matrix_multiply::MatrixF(b_rows, b_cols, b_cols);
-//   h_c = cuda_lab::matrix_multiply::MatrixF(a_rows, b_cols, b_cols);
-
-//   std::vector<float> h_data_a(a_rows * a_cols, 1.0f);
-//   std::vector<float> h_data_b(b_rows * b_cols, 2.0f);
-
-//   std::copy(h_data_a.begin(), h_data_a.end(), h_a.elements.get());
-//   std::copy(h_data_b.begin(), h_data_b.end(), h_b.elements.get());
-
-//   std::fill_n(h_c.elements.get(), h_c.height * h_c.width, 0.0f);
-// }
+constexpr cuda_lab::matrix_multiply::MatrixMultiplyType kMatrixMultiplyType{
+    cuda_lab::matrix_multiply::MatrixMultiplyType::kSharedAB};
 
 void Test1() {
   cuda_lab::matrix_multiply::Matrix a;
@@ -56,15 +38,15 @@ void Test1() {
   c.elements = new float[c.height * c.stride];
   std::fill_n(c.elements, c.height * c.stride, 0.0f);
 
-  cuda_lab::matrix_multiply::MatrixMultiply(
-      a, b, c, cuda_lab::matrix_multiply::MatrixMultiplyType::kSharedAB);
+  cuda_lab::matrix_multiply::MatrixMultiply(a, b, c, kMatrixMultiplyType);
 
   cuda_lab::matrix_multiply::PrintMatrix(c, "C");
 }
 
 void Test2() {
   cuda_lab::matrix_multiply::Matrix a;
-  a.height = 1600;
+  // a.height = 1600;
+  a.height = 16;
   a.width = cuda_lab::matrix_multiply::kBlockSize;
   a.stride = cuda_lab::matrix_multiply::kBlockSize;
   a.elements = new float[a.height * a.stride];
@@ -73,8 +55,10 @@ void Test2() {
 
   cuda_lab::matrix_multiply::Matrix b;
   b.height = cuda_lab::matrix_multiply::kBlockSize;
-  b.width = 1920;
-  b.stride = 1920;
+  b.width = 192;
+  b.stride = 192;
+  // b.width = 192000;
+  // b.stride = 192000;
   b.elements = new float[b.height * b.stride];
   std::vector<float> h_data_b(b.height * b.stride, 2.0f);
   std::copy(h_data_b.begin(), h_data_b.end(), b.elements);
@@ -89,15 +73,14 @@ void Test2() {
   c.elements = new float[c.height * c.stride];
   std::fill_n(c.elements, c.height * c.stride, 0.0f);
 
-  cuda_lab::matrix_multiply::MatrixMultiply(
-      a, b, c, cuda_lab::matrix_multiply::MatrixMultiplyType::kSimple);
+  cuda_lab::matrix_multiply::MatrixMultiply(a, b, c, kMatrixMultiplyType);
 
   cuda_lab::matrix_multiply::PrintMatrix(c, "C");
 }
 
 int main() {
   Test1();
-  //   Test2();
+  // Test2();
 
   return 0;
 }
