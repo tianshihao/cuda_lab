bb7b418e8f7f19da41d01b18c7e19a3208018fb1
Shihao Tian <shihao.tian@outlook.com>
2026-03-06 17:23:15 +0800

    Update memory.hpp

--- include/memory.hpp
+++ include/memory.hpp
@@ -3,57 +3,27 @@
 #include <cuda_runtime.h>
 
 #include <cstddef>
-#include <cstdint>  // for std::byte
-#include <iostream>
-#include <stdexcept>
+
+#include "allocator.hpp"
+#include "cuda_check.hpp"
+#include "initializer.hpp"
+#include "memory_type.hpp"
 
 namespace cuda_lab {
-
-enum class MemoryType { kHost = 0, kPinned, kDevice, kMappedPinned };
 
 template <typename T, MemoryType Type>
 class Memory {
  public:
-  explicit Memory(std::size_t n, T value = T{})
-      : n_{n}, raw_{nullptr}, bytes_{n * sizeof(T)} {
-    if constexpr (Type == MemoryType::kPinned) {
-      void* tmp{nullptr};
-      if (cudaHostAlloc(&tmp, bytes_, cudaHostAllocDefault) != cudaSuccess) {
-        throw std::runtime_error("cudaHostAlloc failed");
-      }
-      raw_ = static_cast<std::byte*>(tmp);
-    } else if constexpr (Type == MemoryType::kDevice) {
-      void* tmp{nullptr};
-      if (cudaMalloc(&tmp, bytes_) != cudaSuccess) {
-        throw std::runtime_error("cudaMalloc (device) failed");
-      }
-      raw_ = static_cast<std::byte*>(tmp);
-    } else if constexpr (Type == MemoryType::kMappedPinned) {
-      void* tmp{nullptr};
-      if (cudaHostAlloc(&tmp, bytes_, cudaHostAllocMapped) != cudaSuccess) {
-        throw std::runtime_error("cudaHostAlloc (mapped) failed");
-      }
-      raw_ = static_cast<std::byte*>(tmp);
-    } else {
-      raw_ = static_cast<std::byte*>(operator new[](bytes_));
-    }
-
-    fill(value);
+  explicit Memory(std::size_t const& n, T const& initial_value = T{})
+      : n_{n},
+        raw_{reinterpret_cast<std::byte*>(
+            MemoryAllocator<T, Type>::allocate(n))},
+        bytes_{n * sizeof(T)} {
+    fill(initial_value);
   }
 
   ~Memory() {
-    if constexpr (Type == MemoryType::kPinned ||
-                  Type == MemoryType::kMappedPinned) {
-      if (raw_) {
-        cudaFreeHost(raw_);
-      }
-    } else if constexpr (Type == MemoryType::kDevice) {
-      if (raw_) {
-        cudaFree(raw_);
-      }
-    } else {
-      operator delete[](raw_);
-    }
+    MemoryAllocator<T, Type>::deallocate(reinterpret_cast<T*>(raw_));
   }
 
   Memory(const Memory&) = delete;
@@ -65,16 +35,10 @@ class Memory {
     other.n_ = 0;
     other.bytes_ = 0;
   }
+
   Memory& operator=(Memory&& other) noexcept {
     if (this != &other) {
-      if constexpr (Type == MemoryType::kPinned ||
-                    Type == MemoryType::kMappedPinned) {
-        if (raw_) {
-          cudaFreeHost(raw_);
-        }
-      } else {
-        operator delete[](raw_);
-      }
+      MemoryAllocator<T, Type>::deallocate(reinterpret_cast<T*>(raw_));
       raw_ = other.raw_;
       n_ = other.n_;
       bytes_ = other.bytes_;
@@ -85,100 +49,27 @@ class Memory {
     return *this;
   }
 
-  T* host_ptr() {
-    if constexpr (Type == MemoryType::kHost || Type == MemoryType::kPinned ||
-                  Type == MemoryType::kMappedPinned) {
-      return reinterpret_cast<T*>(raw_);
-    } else {
-      return nullptr;
-    }
+  void fill(T const& value) {
+    MemoryInitializer<T, Type>::fill(data(), n_, value);
   }
-  T const* host_ptr() const {
-    if constexpr (Type == MemoryType::kHost || Type == MemoryType::kPinned ||
-                  Type == MemoryType::kMappedPinned) {
-      return reinterpret_cast<const T*>(raw_);
-    } else {
-      return nullptr;
-    }
-  }
-  T* data() {
-    if constexpr (Type == MemoryType::kDevice ||
-                  Type == MemoryType::kMappedPinned ||
-                  Type == MemoryType::kPinned) {
-      return device_ptr();
-    } else {
-      return host_ptr();
-    }
-  }
-  T const* data() const {
-    if constexpr (Type == MemoryType::kDevice ||
-                  Type == MemoryType::kMappedPinned ||
-                  Type == MemoryType::kPinned) {
-      return device_ptr();
-    } else {
-      return host_ptr();
-    }
-  }
+
+  T* data() { return reinterpret_cast<T*>(raw_); }
+  T const* data() const { return reinterpret_cast<const T*>(raw_); }
   std::size_t size() const { return n_; }
   std::size_t bytes() const { return bytes_; }
-  void fill(T value) {
-    if constexpr (Type == MemoryType::kDevice) {
-      if constexpr (std::is_same_v<T, uint8_t> || std::is_same_v<T, char> ||
-                    std::is_same_v<T, std::byte>) {
-        cudaMemset(raw_, static_cast<int>(value), bytes_);
-      } else {
-        auto* tmp{new T[n_]};
-        std::fill(tmp, tmp + n_, value);
-        cudaMemcpy(raw_, tmp, bytes_, cudaMemcpyHostToDevice);
-        delete[] tmp;
-      }
+
+  template <MemoryType OtherType>
+  void copy_to(Memory<T, OtherType>& dst) const {
+    if constexpr (Type == MemoryType::kHost &&
+                  OtherType == MemoryType::kDevice) {
+      CUDA_CHECK(cudaMemcpy(dst.data(), this->data(), this->size() * sizeof(T),
+                            cudaMemcpyHostToDevice));
+    } else if constexpr (Type == MemoryType::kDevice &&
+                         OtherType == MemoryType::kHost) {
+      CUDA_CHECK(cudaMemcpy(dst.data(), this->data(), this->size() * sizeof(T),
+                            cudaMemcpyDeviceToHost));
     } else {
-      std::fill(data(), data() + n_, value);
-    }
-  }
-  T* slice(std::size_t offset) { return data() + offset; }
-  T* device_ptr() {
-    if constexpr (Type == MemoryType::kMappedPinned) {
-      T* dev_ptr{nullptr};
-      if (cudaHostGetDevicePointer(&dev_ptr, raw_, 0) != cudaSuccess) {
-        throw std::runtime_error("cudaHostGetDevicePointer failed");
-      }
-      return dev_ptr;
-    } else if constexpr (Type == MemoryType::kDevice) {
-      return data();
-    } else if constexpr (Type == MemoryType::kPinned) {
-      return data();
-    } else if constexpr (Type == MemoryType::kHost) {
-      return data();
-    } else {
-      static_assert(
-          Type == MemoryType::kMappedPinned || Type == MemoryType::kDevice ||
-              Type == MemoryType::kPinned || Type == MemoryType::kHost,
-          "device_ptr() is only valid for mapped pinned, device, pinned, or "
-          "host memory");
-      return nullptr;
-    }
-  }
-  const T* device_ptr() const {
-    if constexpr (Type == MemoryType::kMappedPinned) {
-      T* dev_ptr{nullptr};
-      if (cudaHostGetDevicePointer(&dev_ptr, raw_, 0) != cudaSuccess) {
-        throw std::runtime_error("cudaHostGetDevicePointer failed");
-      }
-      return dev_ptr;
-    } else if constexpr (Type == MemoryType::kDevice) {
-      return data();
-    } else if constexpr (Type == MemoryType::kPinned) {
-      return data();
-    } else if constexpr (Type == MemoryType::kHost) {
-      return data();
-    } else {
-      static_assert(
-          Type == MemoryType::kMappedPinned || Type == MemoryType::kDevice ||
-              Type == MemoryType::kPinned || Type == MemoryType::kHost,
-          "device_ptr() is only valid for mapped pinned, device, pinned, or "
-          "host memory");
-      return nullptr;
+      // Add more cases as needed
     }
   }
 
